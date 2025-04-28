5f3f8d5ddec803330841415c4a5892daf336cda0
SaintSmok <saintmsoku@gmail.com>
2025-04-28 16:43:17 +0800

    43-r hicheeliih hiisen

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Тоглогчийн ээлжийг хадгөлөх хувьсагч
 
-var activePlayer = 1
+var activePlayer = 0
 
 // Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
 
@@ -12,29 +12,63 @@ var roundScore = 0;
 
 // Шооны аль талаараа буусанг хадгалах хувьсагч /1 - 6/
 
-var dice = Math.floor(Math.random() * 6) + 1;
-
 /* <div class="player-score" id="score-0">43</div> */
 
 // Тоглогчдийн цуглуулсан оноо
 
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
 
+// document.querySelector("#score-0").textContent = 0;
+// document.querySelector("#score-1").textContent = 0;
 
-// window.document.querySelector("#score-1").innerHTML = "<em>YES!<em>";
+// document.querySelector("#score-1").innerHTML = "<em>YES!<em>";
 
 /* <img src="./src/dice-2.png" alt="Dice" class="dice"></img> */
 
 // Шоог байхгүй болгож туршив.
-window.document.querySelector(".dice").style.display = "none";
+document.querySelector(".dice").style.display = "none";
 
 // Тоглогчдийн цуглуулж буй оноо
 
 /* <div class="player-current-score" id="current-0">11</div> */
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
 
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
+// document.querySelector("#current-0").textContent = 0;
+// document.querySelector("#current-1").textContent = 0;
 
-console.log("Шоо " + dice + " талаараа буулаа.");
+// <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
+
+var diceDom = document.querySelector(".dice");
+
+document.querySelector(".btn-roll").addEventListener("click", function(e) {
+    // Шооны буусан тал
+    var dice = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "./src/dice-" + dice + ".png";
+    
+    console.log("Шоо " + dice + " талаараа буулаа.");
+
+    // 1-ээс ялгаатөай үед оноог нэмнэ.
+
+    if (dice !== 1) {
+        roundScore = roundScore + dice;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        // 1 буусан тохиолдолд тухайн тоглогчийн цуглуулж буй оноог 0 болгоно.
+        // Тоглогчийн ээлжийг нөгөө тоглогчид шилжүүлнэ.
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        // if (activePlayer === 0){activePlayer = 1;}
+        // else {activePlayer = 0;}
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        // 1 буусан тохиолдолд шоог харуулахгүй.
+        diceDom.style.display = "none";
+
+    }
+})
