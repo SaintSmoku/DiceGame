42dbe31576cb06033986499b67d7d9c70415f345
SaintSmok <saintmsoku@gmail.com>
2025-04-29 16:04:30 +0800

    Togloom duussan

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 // Тоглогчийн ээлжийг хадгөлөх хувьсагч
-
-var activePlayer = 0
+var activePlayer;
 
 // Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
-
-var scores = [0, 0];
+var scores;
 
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-
-var roundScore = 0;
-var winScore = 10;
+var roundScore;
+var winScore;
 
 // Ялагч тодорсон эсэх
 
-var winner = false;
+var winner;
 
 // Шооны аль талаараа буусанг хадгалах хувьсагч /1 - 6/
 
 /* <div class="player-score" id="score-0">43</div> */
 
 // Тоглогчдийн цуглуулсан оноо
-
-document.getElementById("score-0").textContent = 0;
-document.getElementById("score-1").textContent = 0;
 
 // document.querySelector("#score-0").textContent = 0;
 // document.querySelector("#score-1").textContent = 0;
@@ -38,19 +32,41 @@ document.querySelector(".dice").style.display = "none";
 
 /* <div class="player-current-score" id="current-0">11</div> */
 
-document.getElementById("current-0").textContent = 0;
-document.getElementById("current-1").textContent = 0;
-
 // document.querySelector("#current-0").textContent = 0;
 // document.querySelector("#current-1").textContent = 0;
 
 // <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
 
 var diceDom = document.querySelector(".dice");
-console.log(scores[1]);
+
+initGame();
 
 // Ялагчийг тодруулах.
 
+function initGame(){
+    activePlayer = 0;
+    winner = false;
+    roundScore = 0;
+    scores = [0, 0];
+    winScore = 10;
+
+    document.querySelector(".dice").style.display = "none";
+
+    document.getElementById("score-0").textContent = 0;
+    document.getElementById("score-1").textContent = 0;
+
+    document.getElementById("current-0").textContent = 0;
+    document.getElementById("current-1").textContent = 0;
+
+    document.getElementById("name-0").textContent = "Player 1";
+    document.getElementById("name-1").textContent = "Player 2";
+
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+
+    document.querySelector(".player-0-panel").classList.add("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+}
 
 document.querySelector(".btn-roll").addEventListener("click", function(e) {
 
@@ -59,12 +75,10 @@ document.querySelector(".btn-roll").addEventListener("click", function(e) {
     var dice = Math.floor(Math.random() * 6) + 1;
     diceDom.style.display = "block";
     diceDom.src = "./src/dice-" + dice + ".png";
-
-    console.log(winner);
     
     // console.log("Шоо " + dice + " талаараа буулаа.");
 
-    // 1-ээс ялгаатөай үед оноог нэмнэ.
+    // 1-ээс ялгаатай үед оноог нэмнэ.
 
     if (dice !== 1) {
         roundScore = roundScore + dice;
@@ -75,8 +89,7 @@ document.querySelector(".btn-roll").addEventListener("click", function(e) {
         changeState();
 
         // 1 буусан тохиолдолд шоог харуулахгүй.
-        diceDom.style.display = "none";
-
+        
     }
 }else {
     alert(`Ялагч тодорсон байна. Дахин эхлүүлэхийн тулд "NEW GAME" товчийг дарна уу.`);
@@ -88,13 +101,16 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
     
     if (winner !== true) {
     scores[activePlayer] += roundScore;
-    document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+    
     if (scores[activePlayer] > winScore){
         document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
         document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
         winner = true; 
+    } else {
+        document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
+        changeState();
     }
-    changeState();
+   
     }else {
         alert(`Ялагч тодорсон байна. Дахин эхлүүлэхийн тулд "NEW GAME" товчийг дарна уу.`);
     }
@@ -103,32 +119,20 @@ document.querySelector(".btn-hold").addEventListener("click", function() {
 // .winner { background-color: #f7f7f7; }
 
 {/* <button class="btn-new"><i class="ion-ios-plus-outline"></i>New game</button> */}
-document.querySelector(".btn-new").addEventListener("click", function() {
-    activePlayer = 0
-    scores = [0, 0];
-    roundScore = 0;
-    winner = false;
-    document.getElementById("score-0").textContent = 0;
-    document.getElementById("score-1").textContent = 0;
-    document.querySelector(".dice").style.display = "none";
-    document.getElementById("current-0").textContent = 0;
-    document.getElementById("current-1").textContent = 0;
-} );
+document.querySelector(".btn-new").addEventListener("click", function(){
+    initGame();
+});
 
 // DRY буюу нэмэлт функцүүд
 
 function changeState (){
     roundScore = 0;
-        document.getElementById("current-" + activePlayer).textContent = 0;
-        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-        // if (activePlayer === 0){activePlayer = 1;}
-        // else {activePlayer = 0;}
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // if (activePlayer === 0){activePlayer = 1;}
+    // else {activePlayer = 0;}
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
 
-        console.log(scores);
-}
-
-function checkWinner(){
-
+    diceDom.style.display = "none";
 }
